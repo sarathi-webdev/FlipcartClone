ebf6e493fcccfa015ebb45ef853bf16b2cc5ebbe
sara-lek <sarathisasikala2006@gmail.com>
2026-05-09 17:00:02 +0530

    increased chinksize

--- Flipcard/vite.config.js
+++ Flipcard/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/FlipcartClone/tree/main/Flipcard"
+  base: process.env.VITE_BASE_PATH || "/FlipcartClone/tree/main/Flipcard",
+   build: {
+    chunkSizeWarningLimit: 1000
+  }
+
 })
